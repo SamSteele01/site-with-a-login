1af7be59bd48883a74dd6887d3c1d86f9888d2e8
Sam Steele <samsteele@User-Mac.local>
2017-08-23 14:07:50 -0400

    Added page visit counter

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
 const mustache = require('mustache-express');
-// const loginRouter = require('./routes/login');
-const User = require('./users.js');
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const parseurl = require('parseurl')
+const path = require('path');
+const app = express();
+const port = 3000;
+const User = require('./users.js');
 
 app.engine('mustache', mustache());
 app.set('views', ['./views', './views/login', './views/signup']);
@@ -19,13 +19,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
-app.set('trust proxy', 1) // trust first proxy
+// app.set('trust proxy', 1) // trust first proxy
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
   // cookie: { secure: true }
 }))
+
+app.use(function (req, res, next) {
+  var views = req.session.views
+  if (!views) {
+    views = req.session.views = {}
+  }
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+  // count the views
+  views[pathname] = (views[pathname] || 0) + 1
+  next()
+})
 
 var errors = [];
 var authErrors = "";
@@ -39,9 +51,11 @@ app.get('/', function(req,res){
 
 app.get('/index', function(req, res) {
 //   // redirect to login if not already logged in
-//   var user = User.find()
-//   res.send(user);
+  if(req.session.authenticated){
+    res.render('index', { username: req.session.username, token: req.session.cookie, views: req.session.views['/index']});
+  }else{
   res.redirect('/login')
+  }
 })
 
 app.get('/login', function(req,res){
@@ -52,21 +66,17 @@ app.post('/login', function(req,res){
   errors = [];
   var username = req.body.username;
   var password = req.body.password;
-  // checkEmpty(req, username, password);
-  console.log(User.getUser(username));
-  // console.log(User.find(username));
   if(username===""){
     errors.push("You must enter a username.")
   }
-  console.log(User.getPass(password));
-  // console.log(User.find(password));
   if(password===""){
     errors.push("You must enter a password.")
   }
   if(username!=""&&password!=""){
     authenticate(req, username, password);
     if (req.session && req.session.authenticated){
-      res.render('index', { username: username });
+      req.session.username = username;
+      res.redirect('/index');
     } else {
       errors.push("Unable to authenticate user.");
       res.redirect('/');
@@ -79,6 +89,7 @@ function authenticate(req, username, password){
   var authUser = User.userList.find(function (user) {
     if (username === user.username && password === user.password) {
       req.session.authenticated = true;
+
       console.log('User & Password Authenticated');
     } else {
       // errors.push("Could not authenticate. ");
@@ -89,6 +100,7 @@ function authenticate(req, username, password){
   return req.session;
 };
 
+// not needed
 function checkEmpty(req, username, password){
   req.checkBody("username", "You must enter a username!").notEmpty();
   req.getValidationResult().then(function(result) {
@@ -120,10 +132,6 @@ function checkEmpty(req, username, password){
     }
     return eErrors;
 })};
-
-// app.get('/login', function(req,res){
-//   res.render('login');
-// })
 
 app.listen(port, function() {
   console.log('Example listening on port 3000')
